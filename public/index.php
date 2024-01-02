28402c3d6e05c74b72e8d157b44d5243a27403f9
Matteo Grado <111707247+MatteoGrado@users.noreply.github.com>
2024-01-02 15:30:20 +0100

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tech-Paradies.de</title>
+    <link rel="stylesheet" href="assets/css/start/style.css">
+</head>
+<body>
+    <div class="header">
+        <h1 class="header1">
+            Willkommen
+        </h1>
+        <div class="nav">
+            <a href="#">Produkte</a>
+            <a href="#">Marken</a>
+            <a href="#">Support</a>
+            <a href="#">&Uuml;ber Uns</a>
+            <a href="#">Kontakt</a>
+            <a href="#">Login</a>
+        </div>
+    </div>
+
+    <div class="main-container">
+        <div class="container">
+            <img src="assets/img/jpg/01.jpg" alt="" class="img">
+            <p class="description">
+                Laptop Dell 15 Zoll<br>
+                RAM: 16GB &amp;<br>
+                Speicher: 1TB NVME SSD<br><br>
+                Preis: 847.45&euro;
+            </p>
+            <button type="submit" class="btn1">Jetzt kaufen</button>
+            <button type="submit" class="btn2">Warenkorb</button>
+        </div>
+        <div class="container">
+            <img src="assets/img/jpg/01.jpg" alt="" class="img">
+            <p class="description">
+                Laptop Dell 15 Zoll<br>
+                RAM: 16GB &amp;<br>
+                Speicher: 1TB NVME SSD<br><br>
+                Preis: 847.45&euro;
+            </p>
+            <button type="submit" class="btn1">Jetzt kaufen</button>
+            <button type="submit" class="btn2">Warenkorb</button>
+        </div>
+        <div class="container">
+            <img src="assets/img/jpg/01.jpg" alt="" class="img">
+            <p class="description">
+                Laptop Dell 15 Zoll<br>
+                RAM: 16GB &amp;<br>
+                Speicher: 1TB NVME SSD<br><br>
+                Preis: 847.45&euro;
+            </p>
+            <button type="submit" class="btn1">Jetzt kaufen</button>
+            <button type="submit" class="btn2">Warenkorb</button>
+        </div>
+        <div class="container">
+            <img src="assets/img/jpg/01.jpg" alt="" class="img">
+            <p class="description">
+                Laptop Dell 15 Zoll<br>
+                RAM: 16GB &amp;<br>
+                Speicher: 1TB NVME SSD<br><br>
+                Preis: 847.45&euro;
+            </p>
+            <button type="submit" class="btn1">Jetzt kaufen</button>
+            <button type="submit" class="btn2">Warenkorb</button>
+        </div>
+    </div>
+    <div class="main-container1">
+        <div class="container">
+            <img src="assets/img/jpg/01.jpg" alt="" class="img">
+            <p class="description">
+                Laptop Dell 15 Zoll<br>
+                RAM: 16GB &amp;<br>
+                Speicher: 1TB NVME SSD<br><br>
+                Preis: 847.45&euro;
+            </p>
+            <button type="submit" class="btn1">Jetzt kaufen</button>
+            <button type="submit" class="btn2">Warenkorb</button>
+        </div>
+        <div class="container">
+            <img src="assets/img/jpg/01.jpg" alt="" class="img">
+            <p class="description">
+                Laptop Dell 15 Zoll<br>
+                RAM: 16GB &amp;<br>
+                Speicher: 1TB NVME SSD<br><br>
+                Preis: 847.45&euro;
+            </p>
+            <button type="submit" class="btn1">Jetzt kaufen</button>
+            <button type="submit" class="btn2">Warenkorb</button>
+        </div>
+        <div class="container">
+            <img src="assets/img/jpg/01.jpg" alt="" class="img">
+            <p class="description">
+                Laptop Dell 15 Zoll<br>
+                RAM: 16GB &amp;<br>
+                Speicher: 1TB NVME SSD<br><br>
+                Preis: 847.45&euro;
+            </p>
+            <button type="submit" class="btn1">Jetzt kaufen</button>
+            <button type="submit" class="btn2">Warenkorb</button>
+        </div>
+        <div class="container">
+            <img src="assets/img/jpg/01.jpg" alt="" class="img">
+            <p class="description">
+                Laptop Dell 15 Zoll<br>
+                RAM: 16GB &amp;<br>
+                Speicher: 1TB NVME SSD<br><br>
+                Preis: 847.45&euro;
+            </p>
+            <button type="submit" class="btn1">Jetzt kaufen</button>
+            <button type="submit" class="btn2">Warenkorb</button>
+        </div>
+    </div>
+    <?php
+        //Inhalt
+    ?>
+    <div class="copyright">
+        &reg; 02-01-2024 - Alle rechte vorbehalten!
+    </div>
+</body>
+</html>
